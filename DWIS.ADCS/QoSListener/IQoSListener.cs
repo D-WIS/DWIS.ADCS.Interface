48f0c9d45c138ec7edd9993889ddc3fc976830bb
metasong <metaseed@gmail.com>
2023-07-27 16:23:31 -0500

    note

--- DWIS.ADCS/QoSListener/IQoSListener.cs
+++ DWIS.ADCS/QoSListener/IQoSListener.cs
@@ -3,14 +3,15 @@
 public interface IQoSListener : IListener
 {
 	// Notification that connectivity has been dropped.
+	// Mechanism to notify a subscriber that a producer of data is no longer valid. Does not tell us why the DataWriter is no longer available.
 	void LivelinessLost(LivelinessLostStatus status);
-	// When New Data is available
+	//  Mechanism to notify a subscriber when new data is available for reading.
 	void DataAvailable(DataAvailableStatus status);
-	// Notification that publication deadline is missed
+	// Mechanism to notify a subscriber that the expected deadline for a new data sample has expired.
 	void DeadlineMissed(DeadlineMissedStatus status);
-	// Lifetime of data has expired
+	// Mechanism to notify a subscriber that the DataWriter has disposed of the topic instance. This instance will never be seen again
 	void DataDisposed();
-	// New publisher available in the system
+	// Mechanism to notify a subscriber that a new publisher is available.
 	void OnPublicationMatched(PublicationMatchedStatus status);
 	// New subscriber available in the system
 	void OnSubscriptionMatched(SubscriptionMatchedStatus status);
