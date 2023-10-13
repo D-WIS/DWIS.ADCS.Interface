415055f6e75a27a11f81e9bf408163cff1c60168
metasong <metaseed@gmail.com>
2023-10-13 18:04:34 +0200

    script

---
+++ OpcUaRef/OpcUa.Driver.TestServer/scripts/lib/msbuild.ps1
@@ -0,0 +1,7 @@
+#old way
+# Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+# Enter-VsDevShell -InstanceId f86c8b33
+$VsWherePath = "`"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe`""
+$config = Invoke-Expression "& $VsWherePath -latest -format json" | ConvertFrom-Json
+$base = $config.InstallationPath
+. "$base\Common7\Tools\Launch-VsDevShell.ps1"
