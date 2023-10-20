6994810dabae2f541aca60e9f692b406feef61bb
metasong <metaseed@gmail.com>
2023-10-20 15:13:22 +0200

    register reading

--- OpcUaRef/OpcUa.Driver/Client/IOpcUaClient.cs
+++ OpcUaRef/OpcUa.Driver/Client/IOpcUaClient.cs
@@ -8,6 +8,6 @@ public partial interface IOpcUaClient
 	ISession Session { get; }
 
 	Task SubscribeAsync(IEnumerable<SubscriptionNode> nodes, int publishingInterval, CancellationToken token = default);
-	Task ReadCyclicAsync(IEnumerable<NodeId> nodes, int interval, Action<DataValueCollection> handler, CancellationToken token = default);
+	Task ReadCyclicAsync(IEnumerable<NodeId> nodes, int interval, Action<DataValueCollection> handler, bool register = true, CancellationToken token = default);
 
 }
--- OpcUaRef/OpcUa.Driver/Client/OpcUaClient_Read.cs
+++ OpcUaRef/OpcUa.Driver/Client/OpcUaClient_Read.cs
@@ -5,21 +5,34 @@ namespace OpcUa.Driver.Client;
 
 public partial class OpcUaClient
 {
-	readonly List<MmTimer> _timers = new (); // to keep reference of timers
-	public async Task ReadCyclicAsync(IEnumerable<NodeId> nodes, int interval, Action<DataValueCollection> handler, CancellationToken token = default)
+	readonly List<MmTimer> _timers = new(); // to keep reference of timers
+	public async Task ReadCyclicAsync(IEnumerable<NodeId> nodes, int interval, Action<DataValueCollection> handler, bool register = true, CancellationToken token = default)
 	{
+		var nds = nodes.ToArray();
+		var ids = NodeIdCollection.ToNodeIdCollection(nds);
+		if (register)
+		{
+			var resp = await Session.RegisterNodesAsync(null, ids, token).ConfigureAwait(false);
+			ids = resp.RegisteredNodeIds;
+		}
+
+
 		var timer = new MmTimer(interval);
 		_timers.Add(timer);
 		timer.Elapsed += async (o, e) =>
 		{
-			var result = await Session.ReadValuesAsync(nodes.ToList(), token).ConfigureAwait(false);
+			var result = await Session.ReadValuesAsync(ids, token).ConfigureAwait(false);
 			handler(result.Item1);
 		};
 		timer.Start();
-		token.Register(()=>
+		token.Register(() =>
 		{
 			timer.Stop();
 			_timers.Remove(timer);
+			if (register)
+			{
+				Session.UnregisterNodesAsync(null, ids, token);
+			}
 		});
 
 	}
